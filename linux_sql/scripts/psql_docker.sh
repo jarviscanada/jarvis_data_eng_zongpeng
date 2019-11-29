4deff40fcd7b88dfcf0434343cb25486076c440f
yangzongpeng <yangzongpeng@gmail.com>
2019-11-29 19:22:36 +0000

    update- new requirements

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,24 +1,57 @@
 # script usage 
 # ./scripts/psql_docker.sh start|stop [password for database]
 
+dockerrun () {
+	docker run --name jrvs-psql -e POSTGRES_PASSWORD=$2 -d -v \
+	pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
+	exit 0
+}
+
+
 if (($#>2));
 then
-	echo "You have entered too many arguments!"
-	echo "Format: psql_docker.sh start [password] OR psql_docker.sh stop"
+	echo "WARNING: You have entered too many arguments...
+Format: psql_docker.sh start [password] OR psql_docker.sh stop" >&2
+	exit 1
+elif [[ "$1" == "start" && "$#" == 1 ]];
+then
+	echo "WARNING: You did not enter password..." >&2
 	exit 1
 fi
+
 
 if [ "$1" == "start" ];
 then
 	systemctl status docker || systemctl start docker
-	docker run --rm --name jrvs-psql -e POSTGRES_PASSWORD=$2 -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
-	psql -h localhost -U postgres -W
-elif [ "$1" == "stop" ];
+	if [[ "$(docker ps -f name=jrvs-psql | wc -l)" == 2 ]]
+	then
+		exit 0;
+	fi
+
+	if [[ "$(docker volume ls | egrep "pgdata1$" | wc -l)" == 0 ]];
+	then
+		if [[ "$(docker ps -a -f name=jrvs-psql | wc -l)" == 2 ]];
+		then
+			docker stop jrvs-psql
+			docker container rm jrvs-psql
+		fi
+		docker volume create pgdata
+		dockerrun
+	elif [[ "$(docker container ls -a | egrep "jrvs-psql$" | wc -l)" == 0 ]];
+	then	
+		dockerrun
+	else
+		docker start jrvs-psql
+		exit 0
+	fi
+elif [[ "$1" == "stop" && "$#" == 1 ]];
 then
-	systemctl stop docker
+	systemctl status docker && docker stop jrvs-psql
+	systemctl status docker && systemctl stop docker
+	exit 0
 else
-	echo "The arguments you entered is wrong"
-	echo "Format: psql_docker.sh start [password] OR psql_docker.sh stop"
+	echo "WARNING: The arguments you entered is wrong... 
+Format: psql_docker.sh start [password] OR psql_docker.sh stop" >&2
 	exit 1
 fi
 
