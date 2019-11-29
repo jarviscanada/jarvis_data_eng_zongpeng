0c20811ba8b099b4bf743516b13d456cfa48fe96
ZongpengYang <yangzongpeng@gmail.com>
2019-11-29 14:44:05 -0500

    update queries.sql

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -8,7 +8,7 @@ ORDER BY
 
 
 --Q2--
-
+--create temporary table to sort calculated values
 CREATE TEMP TABLE temp 
 AS
 select info.id as host_id, info.hostname as host_name, 
@@ -20,13 +20,16 @@ RIGHT OUTER JOIN
 host_usage AS usage 
 ON info.id=usage.host_id;
 
+--setting the time interval for timestamp
 ALTER TABLE temp
 ADD COLUMN time_interval timestamp;
 
+--round the time to nearest 5min
 UPDATE temp
 SET time_interval = date_trunc('hour', timestamp) + INTERVAL '5 min' * 
 ROUND( date_part('minute', timestamp) / 5.0);
 
+--create another temporary table to store calculated average over 5 min
 CREATE TEMP TABLE temp_5min 
 AS
 SELECT host_id, host_name, 
@@ -34,6 +37,7 @@ AVG(memory_usage) over (partition by time_interval) as average_usage,
 time_interval, total_mem
 FROM temp;
 
+--print the desired columns
 SELECT host_id, host_name, total_mem, AVG(average_usage)
 FROM temp_5min 
 GROUP BY host_id, host_name, total_mem;
