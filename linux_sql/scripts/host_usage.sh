82615bf36e4c7ad2bfd2b6adb3781cdb1571f764
ZongpengYang <yangzongpeng@gmail.com>
2019-12-01 18:56:09 -0500

    Updating coding style

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+host_name=$1
+port_number=$2
+database_name=$3
+user_name=$4
+password=$5
+
 if (($#!=5));
 then
 	echo "WARNING: The number of arguments is not correct... Please enter exact 5 arguments.
-Format: host_info.sh host_name port_number database_name user_name password >&2
+Format: host_usage.sh host_name port_number database_name user_name password >&2
 	exit 1
 fi
 
@@ -15,16 +21,16 @@ cpu_idel=$(vmstat --unit M | awk '{for(i=NF;i>0;i--)if($i=="id"){x=i;break}}END{
 cpu_kernel=$(vmstat --unit M | awk '{for(i=NF;i>0;i--)if($i=="sy"){x=i;break}}END{print $x}')
 disk_io=$(vmstat -D | egrep "inprogress\sIO" | awk '{print $1}')
 disk_available=$(df -BM / | awk '{for(i=NF;i>0;i--)if($i=="Available"){x=i;break}}END{print $x}' | grep -o "[0-9]\+")
-host_name=$(hostname -f)
+hostName=$(hostname -f)
 
 # insert collected information into the table created
 # host_id is from host_info table determined by the hostname
-psql -h $1 -U $4 -w $3 -p $2 -c \
+psql -h $host_name -U $user_name -w $database_name -p $port_number -c \
 	"INSERT INTO host_usage
 	 (timestamp, host_id, memory_free, cpu_idel, cpu_kernel, disk_io,
 	  disk_available)
 	 VALUES ('$timestamp', 
- 		(SELECT id from host_info where hostname='$host_name'),
+ 		(SELECT id from host_info where hostname='$hostName'),
 		$memory_free, $cpu_idel, $cpu_kernel, $disk_io, 
 		$disk_available);"
 
