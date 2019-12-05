50c3442b1b9dd410f9a43c1ca4d6f688ffeeec98
yangzongpeng <yangzongpeng@gmail.com>
2019-12-05 16:36:11 +0000

    Completing grep project

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -3,10 +3,13 @@ package ca.jrvs.apps.grep;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class JavaGrepImp implements JavaGrep {
-
 
     private String rootPath;
     private String regex;
@@ -30,29 +33,68 @@ public class JavaGrepImp implements JavaGrep {
 
     @Override
     public void process() throws IOException {
-
+        List<File> listFile = listFiles(rootPath);
+        List<String> listString = new ArrayList<>();
+        List<String> matchedString = new ArrayList<>();
+        for (File f : listFile) {
+            listString = readLines(f);
+            for (String s : listString) if (constainsPattern(s)) matchedString.add(s);
+        }
+        writeToFile(matchedString);
     }
 
     @Override
     public List<File> listFiles(String rootDir) {
-        return null;
+        File root = new File(rootDir);
+        File[] list = root.listFiles();
+        List<File> fileList = new ArrayList<>();
+        if (list == null) {
+            return null;
+        }
+        for (File f : list) {
+
+            if (f.isDirectory()) {
+                List<File> tempF = listFiles(f.getAbsolutePath());
+                fileList.addAll(tempF);
+            } else {
+                fileList.add(f);
+            }
+        }
+
+        return fileList;
     }
 
     @Override
     public List<String> readLines(File inputFile) {
-        return null;
+        List<String> listString = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(inputFile);
+            while (scanner.hasNextLine()) {
+                listString.add(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listString;
     }
 
     @Override
     public boolean constainsPattern(String line) {
-        return false;
+        return line.matches(this.regex);
     }
 
     @Override
     public void writeToFile(List<String> lines) throws IOException {
+        Path file = (new File(outFile)).toPath();
+        try {
+            Files.createFile(file);
+        } catch (IOException ignored) {
+            ;
+        }
 
+        Files.write(file, lines);
     }
-
 
     @Override
     public String getRootPath() {
