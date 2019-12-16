3494d897adb3f58bba8ac4b363f32f2b04da9388
yangzongpeng <yangzongpeng@gmail.com>
2019-12-16 19:29:43 +0000

    Completing Contorller Implementation...

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/TwitterController.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/TwitterController.java
@@ -1,6 +1,5 @@
 package ca.jrvs.apps.twitter.controller;
 
-import ca.jrvs.apps.twitter.model.Coordinates;
 import ca.jrvs.apps.twitter.model.Tweet;
 import ca.jrvs.apps.twitter.model.TweetConstructor;
 import ca.jrvs.apps.twitter.service.Service;
@@ -9,8 +8,8 @@ import org.springframework.util.StringUtils;
 
 public class TwitterController implements Controller{
 
-  private static String COORD_SEP = ":";
-  private static String COMMA = ",";
+  private static final String COORD_SEP = ":";
+  private static final String COMMA = ",";
 
   private Service service;
 
@@ -26,7 +25,7 @@ public class TwitterController implements Controller{
     }
     String text = args[1];
     if (StringUtils.isEmpty(text)){
-      throw new IllegalArgumentException("Error: The tweet you are trying to post is empty.")
+      throw new IllegalArgumentException("Error: The tweet you are trying to post is empty.");
     }
     String coordinatesRaw = args[2];
     String[] coordinates = coordinatesRaw.split(COORD_SEP);
@@ -43,18 +42,44 @@ public class TwitterController implements Controller{
       throw new IllegalArgumentException("Error: The longitude and latitude you entered contains "
           + "characters.");
     }
-    Tweet tweet = new Tweet();
-    tweet = TweetConstructor.tweetBuild(text, lon, lat);
+    Tweet tweet = TweetConstructor.tweetBuild(text, lon, lat);
     return service.postTweet(tweet);
   }
 
   @Override
   public Tweet showTweet(String[] args) {
-    return null;
+    if (args.length != 3){
+      throw new IllegalArgumentException("Error: Number of arguments is not correct. "
+          + "To post a tweet- USAGE: TwitterCLIApp show \"tweet id\", \"field,field,...\"");
+    }
+    String id = args[1];
+    idChecker(id);
+    String[] fields = args[2].split(COMMA);
+    if (fields.length > 11){
+      throw new IllegalArgumentException("Error: The fields is too many. "
+          + "Please enter up to 11 fields.\n"
+          + "Format: field,field,...");
+    }
+    return service.showTweet(id, fields);
+  }
+
+  private void idChecker(String id) {
+    if (!id.matches("[0-9]+")){
+      throw new IllegalArgumentException("Error: id contains character or space in it. "
+          + "Please make sure the id you entered is correct.");
+    }
   }
 
   @Override
   public List<Tweet> deleteTweet(String[] args) {
-    return null;
+    if (args.length != 2){
+      throw new IllegalArgumentException("Error: Number of arguments is not correct. "
+          + "To post a tweet- USAGE: TwitterCLIApp delete \"tweet_id,tweet_id,...\"");
+    }
+    String[] idArray = args[1].split(COMMA);
+    for (String id : idArray){
+      idChecker(id);
+    }
+    return service.deleteTweets(args);
   }
 }
