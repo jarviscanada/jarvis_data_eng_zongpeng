3ffe46c849bd0517bf64759d6a45a7e9ce4fa34a
yangzongpeng <yangzongpeng@gmail.com>
2020-01-06 15:17:55 +0000

    Completing QuoteTable...

--- springboot/src/main/java/ca/jrvs/apps/trading/service/QuoteService.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/service/QuoteService.java
@@ -14,21 +14,48 @@ public class QuoteService {
 
   private static final Logger logger = LoggerFactory.getLogger(QuoteService.class);
 
-  private IexQuote iexQuote;
+  private QuoteDao quoteDao;
   private MarketDataDao marketDataDao;
 
   @Autowired
-  public QuoteService(IexQuote iexQuote, MarketDataDao marketDataDao){
-    this.iexQuote = iexQuote;
+  public QuoteService(QuoteDao quoteDao, MarketDataDao marketDataDao){
+    this.quoteDao = quoteDao;
     this.marketDataDao = marketDataDao;
   }
 
   /**
    * Find an IexQuote
+   *
+   * @param ticker id
+   * @return IexQuote object
+   * @throws IllegalArgumentException if tikcer is invalid
    */
   public IexQuote findIexQuoteByTicker(String ticker){
     return marketDataDao.findById(ticker)
         .orElseThrow(() -> new IllegalArgumentException(ticker + " is invalid."));
   }
 
+  /**
+   * Update quote table against IEX source
+   *  - get all quotes from the db
+   *  - foreach ticker get IexQuote
+   *  - convert iexQuote to quote entity
+   *  - persist quote to db
+   *
+   * @throws ResourceNotFoundException if ticker is not found from IEX
+   * @throws org.springframework.dao.DataAccessException if unable to retrieve data
+   * @throws IllegalArgumentException for invalid input
+   */
+  public void updateMarketData(){
+
+  }
+
+  /**
+   * Helper method: Map a IexQuote to a Quote entity
+   * Note: `iexQuote.getLastestPrice() == null` if the stock market is closed.
+   * Make sure set a default value for number fields.
+   */
+  protected static Quote buildQuoteFromIexQuote(IexQuote iexQuote){
+
+  }
 }
