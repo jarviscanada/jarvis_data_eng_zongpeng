219a38f7d34dfc8df34b8a00bc6c696f1a327a62
ZongpengYang <yangzongpeng@gmail.com>
2020-02-06 04:35:19 -0500

    Update DashboardController.java

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/DashboardController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/DashboardController.java
@@ -23,10 +23,10 @@ public class DashboardController {
   public DashboardController(DashboardService dashboardService) {
     this.dashboardService = dashboardService;
   }
-/*
+
   @ApiOperation(value = "Show trader profile by trader ID",
       notes = "Show trader and account details. TraderId and AccountId should be identical")
-  @ApiResponses(value = {@ApiResponse(code = 404, message = "traderId or accountId is not found")})*/
+  @ApiResponses(value = {@ApiResponse(code = 404, message = "traderId or accountId is not found")})
   @ResponseBody
   @ResponseStatus(HttpStatus.OK)
   @GetMapping(path = "/profile/traderId/{traderId}", produces = {
@@ -38,9 +38,9 @@ public class DashboardController {
       throw ResponseExceptionUtil.getResponseStatusException(e);
     }
   }
-/*
+
   @ApiOperation(value = "Show portfolio by trader ID")
-  @ApiResponses(value = {@ApiResponse(code = 404, message = "traderId is not found")})*/
+  @ApiResponses(value = {@ApiResponse(code = 404, message = "traderId is not found")})
   @GetMapping(path = "/portfolio/traderId/{traderId}")
   @ResponseStatus(HttpStatus.OK)
   @ResponseBody
